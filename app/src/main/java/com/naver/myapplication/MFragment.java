f61fc8ff5450c2fa0a4d05c163d26174a33d0f37
woohee0817 <woohee0817@naver.com>
2016-12-03 16:08:36 +0900

    no message

---
+++ app/src/main/java/com/naver/myapplication/MFragment.java
@@ -0,0 +1,28 @@
+package com.naver.myapplication;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MFragment extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_m);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            Drawable drawable = getDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp);
+            if (drawable != null) {
+                drawable.setTint(Color.WHITE);
+                actionBar.setHomeAsUpIndicator(drawable);
+            }
+        }
+    }
+}
